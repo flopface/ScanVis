326ec3b8849b711874d423e723d1d084eee29a72
Tom <flopface.nom@gmail.com>
2025-01-28 10:06:43 +0000

    Major changes to class hierarchy structure, all is generalised away from just MRI scans

--- ScanVis/Scanter.py
+++
@@ -1,9 +0,0 @@
-from .Scant import *
-
-class Scanter:
-  def __init__(self, scan_folder, seg_folder):
-    self.scan_folder = scan_folder
-    self.seg_folder = seg_folder
-
-  def __call__(self, scan_file, seg_file = None):
-    return Scant(scan_file = scan_file, seg_file = seg_file, scan_folder = self.scan_folder, seg_folder = self.seg_folder)
